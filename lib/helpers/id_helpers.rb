e7fa412bdda607b4ed4520da022708f32d0e5583
Zac Zheng <zac@zenpow.com>
2011-10-06 15:22:47 +0100

    updated front-end AJAX calling to be Rails 3.0 / UJS and AS 3.0 compatible

--- lib/helpers/id_helpers.rb
+++ lib/helpers/id_helpers.rb
@@ -1,6 +1,6 @@
 module Helpers::IdHelpers
   def list_filter_form_id
-    "#{controller_id}-list_filter-form"
+    "#{controller_id}-list_filter--form"
   end
   def list_filter_input_id(filter)
     "#{controller_id}-list_filter-input[#{filter.filter_type}][#{filter.name}]"
