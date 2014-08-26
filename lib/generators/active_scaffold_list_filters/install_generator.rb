7d8ce73906072c43c44dab1787b0c85b46b7d30a
Adrian Peterson Co <adrianpco@gmail.com>
2014-08-26 20:42:43 +0800

    Adds blank generators

---
+++ lib/generators/active_scaffold_list_filters/install_generator.rb
@@ -0,0 +1,12 @@
+require 'rails/generators'
+
+module ActiveScaffoldListFilters
+  class InstallGenerator < Rails::Generators::Base
+    desc "Installation for ActiveScaffoldListFilters"
+
+    def install
+
+    end
+
+  end
+end
---
+++ lib/generators/active_scaffold_list_filters/uninstall_generator.rb
@@ -0,0 +1,12 @@
+require 'rails/generators'
+
+module ActiveScaffoldListFilters
+  class UninstallGenerator < Rails::Generators::Base
+    desc "Uninstallation for ActiveScaffoldListFilters"
+
+    def uninstall
+
+    end
+
+  end
+end
