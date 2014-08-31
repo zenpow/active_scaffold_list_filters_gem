58adebefc127e9a524859ddcf773d12268f85ce7
Adrian Peterson Co <adrianpco@gmail.com>
2014-08-31 19:14:33 +0800

    Moves init.rb and environment.rb to proper locations

--- lib/active_scaffold_list_filters.rb
+++ lib/active_scaffold_list_filters.rb
@@ -1,6 +1,20 @@
 require 'active_scaffold_list_filters/version'
-
+require 'active_scaffold_list_filters/active_scaffold/'
+require 'active_scaffold_list_filters/active_scaffold/actions/'
+require 'active_scaffold_list_filters/active_scaffold/config/'
+require 'active_scaffold_list_filters/active_scaffold/data_structures/'
+require 'active_scaffold_list_filters/active_scaffold/helpers/'
+require 'active_scaffold_list_filters/extensions/'
+require 'active_scaffold_list_filters/list_filters/'
 
 module ActiveScaffoldListFilters
+
+  if Rails::VERSION::MAJOR <= 3
+    raise "This version of ActiveScaffoldListFilters requires Rails 3.0 or higher.  Please use an earlier version."
+  else
+    ActiveScaffold::Config::Core.send(:include, Extensions::Core)
+    ActionDispatch::Routing::ACTIVE_SCAFFOLD_CORE_ROUTING[:collection][:export] = :get
+    ActionView::Base.send(:include, ActiveScaffoldListFilter::ViewHelpers)
+  end
 
 end
