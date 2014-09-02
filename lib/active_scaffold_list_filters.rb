b652e236c1b3a1c83d641610971eee8f3e7f7ab8
Adrian Peterson Co <adrianpco@gmail.com>
2014-09-03 00:56:55 +0800

    Adds asset engine for the gem
    Moves the javascript files to a proper place

--- lib/active_scaffold_list_filters.rb
+++ lib/active_scaffold_list_filters.rb
@@ -1,5 +1,6 @@
 require 'active_scaffold'
 require 'active_scaffold_list_filters/version'
+require 'active_scaffold_list_filters/engine'
 require 'active_scaffold_list_filters/active_scaffold/actions/list_filter'
 require 'active_scaffold_list_filters/active_scaffold/config/list_filter'
 require 'active_scaffold_list_filters/active_scaffold/data_structures/list_filter'
---
+++ lib/active_scaffold_list_filters/engine.rb
@@ -0,0 +1,8 @@
+require 'rails'
+require 'active_scaffold_list_filters'
+
+module ActiveScaffoldListFilters
+  class Engine < Rails::Engine
+
+  end
+end
